17f3d604684b8a0bb789d04449f4ef6b7a478d69
OnyxRader <roundythomas@gmail.com>
2025-01-26 19:24:56 -0700

    added Games to games tab

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+var nothingClicked = 0;
 function PortfolioButton() {
     window.location.href = "portfolio";
 }
 
 function SusButton() {
-    var OMGsfx = new Audio('OMG.mp3');
-    OMGsfx.play();
+    var Jumpscare = new Audio('penguinz0-jumpscare.mp3');
+    Jumpscare.play();
+}
+function GamesButton() {
+    window.location.href = "games";
+}
+function backButton() {
+    window.location.href = "../";
+}
+function NothingButton() {
+    nothingClicked++;
+    if (nothingClicked ==10) {
+        SusButton();
+        document.getElementsByClassName("btn")[2].innerHTML = "Now you are sus";
+    }
 }
